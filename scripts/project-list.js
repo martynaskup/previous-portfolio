6ee89faba3dbc5c908e97c78e2063beca67256cc
Martyna Skup <martyna.skup@yahoo.de>
2019-08-28 22:03:24 +0200

    Update the project links and page

--- scripts/project-list.js
+++ scripts/project-list.js
@@ -3,7 +3,7 @@ const projectContainer = document.querySelector(".grid-container");
 const projectList = [
     {
         title: "Your to-do list",
-        github: "https://github.com/snikerslala/js_todo_tasks",
+        github: "https://snikerslala.github.io/js_todo_tasks/",
         img1: "./images/ToDo.png",
         img2: "./images/ToDo-1.png",
         desc: "Add the tasks you have to do in the future, search and delete them and check which deadlines did you miss already.",
@@ -11,7 +11,7 @@ const projectList = [
     },
     {
         title: "Guest book",
-        github: "https://github.com/snikerslala/guest_book",
+        github: "https://snikerslala.github.io/guest_book/",
         img1: "./images/GuestBook-Entries.png",
         img2: "./images/GuestBook.png",
         desc: "Let your guests to leave comments and display the guest book entries.",
@@ -19,7 +19,7 @@ const projectList = [
     },
     {
         title: "Image Search",
-        github: "https://github.com/snikerslala/image-search",
+        github: "https://snikerslala.github.io/image-search/",
         img1: "./images/ImageSearch.png",
         img2: "./images/ImageSearch-1.png",
         desc: "Looking for a picture? Use this search engine!",
@@ -27,7 +27,7 @@ const projectList = [
     },
     {
         title: "Dancing Robot",
-        github: "https://github.com/snikerslala/60_dancing_robot",
+        github: "https://snikerslala.github.io/60_dancing_robot/",
         img1: "./images/DancingRobot.png",
         img2: "./images/DancingRobot-1.png",
         desc: "Check out how this robot can dance! You can do the same!",
@@ -35,7 +35,7 @@ const projectList = [
     },
     {
         title: "Robofriends",
-        github: "https://github.com/snikerslala/robofriends",
+        github: "https://snikerslala.github.io/robofriends/",
         img1: "./images/Robofriends.png",
         img2: "./images/Robofriends-1.png",
         desc: "Display and search for Robofriends. <br>This app is build based on React.",
@@ -43,7 +43,7 @@ const projectList = [
     },
     {
         title: "Background Generator",
-        github: "https://github.com/snikerslala/background_generator",
+        github: "https://snikerslala.github.io/background_generator/",
         img1: "./images/BackgroundGenerator.png",
         img2: "./images/BackgroundGenerator-1.png",
         desc: "Generate a gradient background code based on random colors or colors chosen by you!",
