f5738a7326f05fe6bffc8dfd301aece2e70b7925
Martyna Skup <martyna.skup@yahoo.de>
2019-08-29 11:59:04 +0200

    Format the code in the files

--- scripts/project-list.js
+++ scripts/project-list.js
@@ -1,68 +1,72 @@
 const projectContainer = document.querySelector(".grid-container");
 
 const projectList = [
-    {
-        title: "Your to-do list",
-        github: "https://snikerslala.github.io/js_todo_tasks/",
-        img1: "./images/ToDo.png",
-        img2: "./images/ToDo-1.png",
-        desc: "Add the tasks you have to do in the future, search and delete them and check which deadlines did you miss already.",
-        tech: "HTML5, CSS3, JavaScript, <br>LocalStorage API, JSON",
-    },
-    {
-        title: "Guest book",
-        github: "https://snikerslala.github.io/guest_book/",
-        img1: "./images/GuestBook-Entries.png",
-        img2: "./images/GuestBook.png",
-        desc: "Let your guests to leave comments and display the guest book entries.",
-        tech: "HTML5, CSS3, JavaScript, Firebase",
-    },
-    {
-        title: "Image Search",
-        github: "https://snikerslala.github.io/image-search/",
-        img1: "./images/ImageSearch.png",
-        img2: "./images/ImageSearch-1.png",
-        desc: "Looking for a picture? Use this search engine!",
-        tech: "JavaScript, React, unsplash API",
-    },
-    {
-        title: "Dancing Robot",
-        github: "https://snikerslala.github.io/60_dancing_robot/",
-        img1: "./images/DancingRobot.png",
-        img2: "./images/DancingRobot-1.png",
-        desc: "Check out how this robot can dance! You can do the same!",
-        tech: "HTML5, CSS3",
-    },
-    {
-        title: "Robofriends",
-        github: "https://snikerslala.github.io/robofriends/",
-        img1: "./images/Robofriends.png",
-        img2: "./images/Robofriends-1.png",
-        desc: "Display and search for Robofriends. <br>This app is build based on React.",
-        tech: "React, Tachyons, Robo Hash API, JSONPlaceHolder API",
-    },
-    {
-        title: "Background Generator",
-        github: "https://snikerslala.github.io/background_generator/",
-        img1: "./images/BackgroundGenerator.png",
-        img2: "./images/BackgroundGenerator-1.png",
-        desc: "Generate a gradient background code based on random colors or colors chosen by you!",
-        tech: "CSS3, JavaScript",
-    },
+  {
+    title: "Your to-do list",
+    github: "https://snikerslala.github.io/js_todo_tasks/",
+    img1: "./images/ToDo.png",
+    img2: "./images/ToDo-1.png",
+    desc:
+      "Add the tasks you have to do in the future, search and delete them and check which deadlines did you miss already.",
+    tech: "HTML5, CSS3, JavaScript, <br>LocalStorage API, JSON"
+  },
+  {
+    title: "Guest book",
+    github: "https://snikerslala.github.io/guest_book/",
+    img1: "./images/GuestBook-Entries.png",
+    img2: "./images/GuestBook.png",
+    desc:
+      "Let your guests to leave comments and display the guest book entries.",
+    tech: "HTML5, CSS3, JavaScript, Firebase"
+  },
+  {
+    title: "Image Search",
+    github: "https://snikerslala.github.io/image-search/",
+    img1: "./images/ImageSearch.png",
+    img2: "./images/ImageSearch-1.png",
+    desc: "Looking for a picture? Use this search engine!",
+    tech: "JavaScript, React, unsplash API"
+  },
+  {
+    title: "Dancing Robot",
+    github: "https://snikerslala.github.io/60_dancing_robot/",
+    img1: "./images/DancingRobot.png",
+    img2: "./images/DancingRobot-1.png",
+    desc: "Check out how this robot can dance! You can do the same!",
+    tech: "HTML5, CSS3"
+  },
+  {
+    title: "Robofriends",
+    github: "https://snikerslala.github.io/robofriends/",
+    img1: "./images/Robofriends.png",
+    img2: "./images/Robofriends-1.png",
+    desc:
+      "Display and search for Robofriends. <br>This app is build based on React.",
+    tech: "React, Tachyons, Robo Hash API, JSONPlaceHolder API"
+  },
+  {
+    title: "Background Generator",
+    github: "https://snikerslala.github.io/background_generator/",
+    img1: "./images/BackgroundGenerator.png",
+    img2: "./images/BackgroundGenerator-1.png",
+    desc:
+      "Generate a gradient background code based on random colors or colors chosen by you!",
+    tech: "CSS3, JavaScript"
+  }
 ];
 
-const html = (elem) => {
-    const { title, github, img1, img2, desc, tech } = elem;
-    return ` <div class="project">
+const html = elem => {
+  const { title, github, img1, img2, desc, tech } = elem;
+  return ` <div class="project">
         <a href="${github}" target="_blank">
             <h2>${title}</h2>
             <img class="proj-img" src="${img1}" alt="project image">
             <p class="proj-desc">${desc}</p>
             <p class="proj-tech">Tech stack: <br>${tech}</p>
         </a>
-    </div>`
+    </div>`;
 };
 
 for (project of projectList) {
-    projectContainer.innerHTML += html(project);
-};
+  projectContainer.innerHTML += html(project);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,84 +1,80 @@
-const home = document.querySelector('.home');
+const home = document.querySelector(".home");
 const about = document.querySelector(".about");
 const projects = document.querySelector(".projects");
 const contact = document.querySelector(".contact");
 const navi = document.querySelector(".navi");
-const naviList = document.querySelector('.navi-list');
+const naviList = document.querySelector(".navi-list");
 
-    
-    
 /****** SHOW/HIDE SITE SECTIONS *******/
 
 function showHide(e) {
+  const noDisplay = (...hides) => {
+    let hideMe = elem => elem.classList.add("no-display");
+    for (let hide of hides) hideMe(hide);
+  };
 
-    const noDisplay = (...hides) => {
-        let hideMe = elem => elem.classList.add('no-display');
-        for (let hide of hides) hideMe(hide);
+  const visible = (...shows) => {
+    for (show of shows) {
+      if (show.classList.contains("no-display")) {
+        show.classList.remove("no-display");
+      }
+      if (show.classList.contains("hidden")) {
+        show.classList.remove("hidden");
+      }
     }
+  };
 
-    const visible = (...shows) => {
-        for (show of shows) {
-            if (show.classList.contains('no-display')) { 
-                show.classList.remove('no-display');
-            }
-            if (show.classList.contains('hidden')) { 
-                show.classList.remove('hidden');
-            }
-        }
-    };
+  let targetClass = e.target.classList.value;
+  let linkPos = targetClass.indexOf("link");
+  let spacePos = targetClass.indexOf(" ", linkPos);
 
-    let targetClass = e.target.classList.value;
-    let linkPos = targetClass.indexOf("link");
-    let spacePos = targetClass.indexOf(" ", linkPos);
-    
-    let searchClass = ((spacePos == -1)
-        ? targetClass.slice(linkPos,targetClass.length)
-        : targetClass.slice(linkPos,spacePos)
-    );
+  let searchClass =
+    spacePos == -1
+      ? targetClass.slice(linkPos, targetClass.length)
+      : targetClass.slice(linkPos, spacePos);
 
-    switch (searchClass) {
-        case 'link-home': 
-            visible(home);
-            noDisplay(navi, about, projects, contact);
-            naviList.classList.toggle('no-display');
-            break;
-        case 'link-about': 
-            visible(about, navi);
-            noDisplay(projects, contact, home);
-            naviList.classList.toggle('no-display');
-            break;
-        case 'link-projects': 
-            visible(projects, navi);
-            noDisplay(about, contact, home);
-            naviList.classList.toggle('no-display');
-            break;
-        case 'link-contact':
-            visible(contact, navi);
-            noDisplay(about, projects, home);
-            naviList.classList.toggle('no-display');
-            break;
-    };   
-};
+  switch (searchClass) {
+    case "link-home":
+      visible(home);
+      noDisplay(navi, about, projects, contact);
+      naviList.classList.toggle("no-display");
+      break;
+    case "link-about":
+      visible(about, navi);
+      noDisplay(projects, contact, home);
+      naviList.classList.toggle("no-display");
+      break;
+    case "link-projects":
+      visible(projects, navi);
+      noDisplay(about, contact, home);
+      naviList.classList.toggle("no-display");
+      break;
+    case "link-contact":
+      visible(contact, navi);
+      noDisplay(about, projects, home);
+      naviList.classList.toggle("no-display");
+      break;
+  }
+}
 
-navi.addEventListener('click', function(e){
-    if (e.target.classList.contains('bars')) {
-        naviList.classList.toggle('no-display');
-    };
-    if (e.target.parentElement.classList.contains('navi-list')) {
-        showHide(e);
-    };    
-});
-
-home.querySelector('.home-links').addEventListener('click', function(e) {
+navi.addEventListener("click", function(e) {
+  if (e.target.classList.contains("bars")) {
+    naviList.classList.toggle("no-display");
+  }
+  if (e.target.parentElement.classList.contains("navi-list")) {
     showHide(e);
-    naviList.classList.add('no-display');
+  }
 });
 
+home.querySelector(".home-links").addEventListener("click", function(e) {
+  showHide(e);
+  naviList.classList.add("no-display");
+});
 
 // function showHide(e) {
 //     const visible = (show, ...hides) => {
 //         let hideMe = elem => elem.classList.add('no-display');
-//         if (show.classList.contains('no-display')) { 
+//         if (show.classList.contains('no-display')) {
 //             show.classList.remove('no-display');
 //         }
 //         for (let hide of hides) hideMe(hide);
@@ -87,20 +83,20 @@ home.querySelector('.home-links').addEventListener('click', function(e) {
 //     let targetClass = e.target.classList.value;
 //     let linkPos = targetClass.indexOf("link");
 //     let spacePos = targetClass.indexOf(" ", linkPos);
-    
+
 //     let searchClass = ((spacePos == -1)
 //         ? targetClass.slice(linkPos,targetClass.length)
 //         : targetClass.slice(linkPos,spacePos)
 //     );
 
 //     switch (searchClass) {
-//         case 'link-home': 
+//         case 'link-home':
 //             visible(home, about, projects, contact); break;
-//         case 'link-about': 
+//         case 'link-about':
 //             visible(about, projects, contact, home); break;
-//         case 'link-projects': 
+//         case 'link-projects':
 //             visible(projects, about, contact, home); break;
 //         case 'link-contact':
 //             visible(contact, about, projects, home); break;
-//     };   
+//     };
 // };
